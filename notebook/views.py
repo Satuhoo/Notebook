3fff8255b8f1e57a77c22830e2e42bd8136e9b12
Satu <heikkonen.satu@gmail.com>
2020-11-18 14:24:52 +0200

    Added template

--- notebook/views.py
+++ notebook/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from django.shortcuts import render
+from .models import Note
 
 def index(request):
-    return HttpResponse("Hello")
+    latest_notes = Note.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('index.html')
+    context = {
+        'latest_notes': latest_notes,
+    }
+    return HttpResponse(template.render(context, request))
